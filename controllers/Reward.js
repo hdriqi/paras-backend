6f76824021aaba50610af6fbb99a7f9b7b27c772
hdriqi <riqi.asdf@gmail.com>
2020-09-11 20:21:26 +0700

    update daily reward message

--- controllers/Reward.js
+++ controllers/Reward.js
@@ -10,52 +10,61 @@ class Reward {
   }
 
   async disburse() {
-    const self = this
-    const allUsers = await this.ctl().activityPoint.get({})
-    const activeUsers = allUsers.filter(user => {
-      return user.point > 0
-    }).map(user => {
-      user.point = Math.min(user.point, 100)
-      return user
-    }).sort((a, b) => {
-      return b.point - a.point
-    }).sort((a, b) => {
-      return new Date(a.createdAt) - new Date(b.createdAt)
-    })
-
-    const totalDisburse = activeUsers.length * REWARD_PER_USER
-
-    const formula = (i, totalUser) => {
-      return (Math.log10((totalUser + 1) - i) ** 2) / (i ** ((1 / 10) * (Math.abs(1 - (totalUser - i) / totalUser))))
-    }
-
-    let sum = 0
-    for (let i = 0; i < activeUsers.length; i++) {
-      sum += formula(i, activeUsers.length)
-    }
-
-    const k = 100 / sum
-    const rewardsReceive = []
-
-    for (let i = 0; i < activeUsers.length; i++) {
-      const result = formula(i, activeUsers.length)
-      const share = result * k /100
-      rewardsReceive.push({
-        userId: activeUsers[i].userId,
-        point: activeUsers[i].point,
-        share: share,
-        tokens: share * totalDisburse
+    try {
+      const self = this
+      const allUsers = await this.ctl().activityPoint.get({})
+      const activeUsers = allUsers.filter(user => {
+        return user.point > 0
+      }).map(user => {
+        user.point = Math.min(user.point, 100)
+        return user
+      }).sort((a, b) => {
+        return b.point - a.point
+      }).sort((a, b) => {
+        return new Date(a.createdAt) - new Date(b.createdAt)
       })
+  
+      const totalDisburse = activeUsers.length * REWARD_PER_USER
+  
+      const formula = (i, totalUser) => {
+        return (Math.log10((totalUser + 1) - i) ** 2) / (i ** ((1 / 10) * (Math.abs(1 - (totalUser - i) / totalUser))))
+      }
+  
+      let sum = 0
+      for (let i = 0; i < activeUsers.length; i++) {
+        sum += formula(i, activeUsers.length)
+      }
+  
+      const k = 100 / sum
+      const rewardsReceive = []
+  
+      for (let i = 0; i < activeUsers.length; i++) {
+        const result = formula(i, activeUsers.length)
+        const share = result * k /100
+        rewardsReceive.push({
+          userId: activeUsers[i].userId,
+          point: activeUsers[i].point,
+          share: share,
+          tokens: share * totalDisburse
+        })
+      }
+      await this.ctl().wallet.mint('paras::disburse', totalDisburse)
+      await Promise.all(rewardsReceive.map(reward => {
+        return new Promise(async (resolve, reject) => {
+          try {
+            await self.ctl().wallet.internalTransfer('paras::disburse', reward.userId, reward.tokens, `System::RewardDisburse::${new Date().toISOString()}`)
+            resolve(true)
+          } catch (err) {
+            console.log(err)
+            reject(err)
+          }
+        })
+      }))
+      await self.ctl().activityPoint.reset()
+      console.log(rewardsReceive)
+    } catch (err) {
+      console.log(err)
     }
-    await this.ctl().wallet.mint('paras::disburse', totalDisburse)
-    await Promise.all(rewardsReceive.map(reward => {
-      return new Promise(async (resolve, reject) => {
-        await self.ctl().wallet.internalTransfer('paras::disburse', reward.userId, reward.tokens, `Disburse`)
-        resolve()
-      })
-    }))
-    await self.ctl().activityPoint.reset()
-    console.log(rewardsReceive)
   }
 }
 
